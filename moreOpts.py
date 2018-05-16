f110f2c2e946b8b369984992324b2dbad9d80c87
Si Chen <sichen@caper.xray.aps.anl.gov>
2018-05-16 11:15:15 -0500

    added more operations

---
+++ moreOpts.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 16 10:54:54 2018
+
+@author: sichen
+"""
+import sys
+
+# define close
+def close_application(self):
+    sys.exit()
