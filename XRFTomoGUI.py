a03bac3df53b37122dd7bbcfde0aebd6d2096c9a
Si Chen <sichen@caper.xray.aps.anl.gov>
2018-05-17 12:25:26 -0500

    added read hdf

--- XRFTomoGUI.py
+++ XRFTomoGUI.py
@@ -7,6 +7,9 @@ Created on Tue May 15 09:39:53 2018
 """
 
 import sys
+from os.path import isfile, join
+import string
+
 
 try:
     from PyQt4.QtGui import *
@@ -17,13 +20,17 @@ except:
     from PyQt5.QtCore import *
     from PyQt5.QtWidgets import *
 
-from moreOpts import * 
+from moreOpts import *
 
   
 class Window(QMainWindow):
     
     def __init__(self):
         super (Window, self).__init__()
+        self.iniUI()
+        
+    def iniUI(self):
+    
         self.setGeometry(50, 50, 500, 300)
         self.setWindowTitle('XRF Tomo')
         #self.setWindowIcon(QtGui.QIcon(''))
@@ -32,17 +39,18 @@ class Window(QMainWindow):
         
         self.createMenus()
         self.home()
+        self.show()
         
 #main menu    
     def createMenus(self):
         
         opentiffAction = QAction('&Import tiff', self)
         opentiffAction.setStatusTip('Import tiff files')
-        opentiffAction.triggered.connect(self.readDir)
+        opentiffAction.triggered.connect(self.readTiffDir)
         
         openhdfAction = QAction('&Import hdf', self)
         openhdfAction.setStatusTip('Import hdf files')
-        openhdfAction.triggered.connect(self.readDir)
+        openhdfAction.triggered.connect(self.readHDFDir)
         
         saveAction = QAction('&Save', self)
         saveAction.setStatusTip('Save files')
@@ -50,7 +58,7 @@ class Window(QMainWindow):
         extractAction = QAction('&Exit', self)
         extractAction.setShortcut('Ctrl+Q')
         extractAction.setStatusTip('Leave the APP')
-        extractAction.triggered.connect(close_application)
+        extractAction.triggered.connect(self.close_application)
         
         self.statusBar()
         
@@ -68,35 +76,66 @@ class Window(QMainWindow):
         self.editMenu = self.menuBar().addMenu("&Edit")
         
         self.AlignMenu = self.menuBar().addMenu("&Alignment")
-        
-        self.ExtraMenu = self.menuBar().addMenu("&Extra")
          
         self.ReconMenu = self.menuBar().addMenu("&Reconstruction")
+        
+        self.ExtraMenu = self.menuBar().addMenu("&Extra")
         
         
 # add buttons    
     def home(self):
         btn = QPushButton('Quit', self)
-        btn.clicked.connect(close_application)
+        btn.clicked.connect(self.close_application)
         btn.resize(btn.sizeHint())
-        #btn.resize(btn.minimumSizeHint())
         btn.move(100,100)
-        self.show()
+ 
+#for test       
+        testbtn = QPushButton('test', self)
+        testbtn.clicked.connect(test_application)
+        testbtn.resize(btn.sizeHint())
+        #btn.resize(btn.minimumSizeHint())
+        testbtn.move(200,100)
         
-
-
-# readTiffStack
-    def readDir(self):
+    def readTiffDir(self):
         """
         To select the directory of the data
         """ 
-        fileDir = QFileDialog.getExistingDirectory(
-            self, 
-            "Open a folder", 
-            QDir.currentPath(),
-            QFileDialog.ShowDirsOnly
-            )
-        print (fileDir)      
+        path = QFileDialog.getExistingDirectory(self, "Open a folder", 
+                                                   QDir.currentPath())
+             
+    def readHDFDir(self):
+        """
+        To select the directory that contains h5 files
+        """
+        try:
+            path = QFileDialog.getExistingDirectory(self,"Open a folder",
+                                                          QDir.currentPath())                       #type(folderName): <class 'PyQt4.QtCore.QString'
+            global RH,fileName
+            RH = path
+            path = str(path)
+            file_name_array = [f for f in os.listdir(path) if isfile(join(path,f))]                  #a list all the file names 
+            file_name_array = [f for f in os.listdir(path) if string.find(f,"h5")!=-1]               #a list of only h5 file names
+            file_name_array = [path+"/"+f for f in file_name_array]                                  #a list of path/name for the h5 files
+
+            #self.directory = 1
+            #self.fileNames = file_name_array
+            fileName = file_name_array
+            #selectFiles()
+        except IndexError:
+            print "No folder has been selected."
+        except OSError:
+            print "No folder has been selected."
+
+# exit    
+    def close_application(self):
+        choice = QMessageBox.question(self, 'Exit!', 
+                                  'Exiting the progrem?',
+                                  QMessageBox.Yes | QMessageBox.No)
+        if choice == QMessageBox.Yes:
+            print('Exiting now!')
+            sys.exit()
+        else:
+            pass
        
 
 if __name__ == '__main__':
